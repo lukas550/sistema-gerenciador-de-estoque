1c5425f8634c7fd0b93722e987ee17f36d9f91f8
Lukas <xd3433660@gmail.com>
2026-08-04 20:02:14 -0300

    feat: adiciona busca e edições de produtos

--- sistemadeestoque.py
+++ sistemadeestoque.py
@@ -2,10 +2,16 @@ funcionalidades = {
     "1": "Adicionar Produto",
     "2": "Listar Produtos",
     "3": "Buscar Produto",
-    "4": "Atulizar preço/quantidade",
+    "4": "Editar produto",
     "5": "Remover produto",
     "6": "Relátorio Geral",
     "7": 'Sair'
+}
+edicoes = {
+    "1": "Editar preço",
+    "2": "Editar quantidade",
+    "3": "Editar categoria",
+    "4": "Sair"
 }
 
 estoque = {
@@ -24,14 +30,18 @@ produto = {
 def lin(char, qtd=30): # Função organizadora
     return print(char * qtd)
 
-def menu(): # mostrar menu
+def menu(opcoes=False, edicao=True): # mostrar menu
     lin('-')
-    for nu, funcionalidade in funcionalidades.items():
-        print(f'{nu}. {funcionalidade}')
+    if opcoes:
+        for nu, funcionalidade in funcionalidades.items():
+            print(f"{nu}. {funcionalidade}")
+    if edicao:
+        for nu, edit in edicoes.items():
+            print(f"{nu}. {edit}")
     lin('-')
 
 # Funções operacionais
-def adicionar_produto():
+def adicionar_produto(): # Opção 1
         nome_do_produto = input("Digite o nome do produto: ").lower().strip()
 
         if nome_do_produto in estoque:
@@ -56,21 +66,77 @@ def adicionar_produto():
                 print("\nProduto Cadastrado!\n")
                 break
 
-def listar_produtos():
+def listar_produtos(): # Opção 2
     for produto in estoque:
         dados = estoque[produto]
-        print(f"- {produto.capitalize()} / Preço: {dados["preco"]} / Quantidade: {dados["quantidade"]} / Categoria: {dados["categoria"]}")
+        print(f"- {produto.capitalize()} / Preço: {dados['preco']:.2f} / Quantidade: {dados['quantidade']} / Categoria: {dados['categoria']}")
+
+def buscar_produto(): # Opção 3
+    busca = input("Qual produto deseja buscar: ").lower().strip()
+
+    if busca in estoque:
+        dados = estoque[busca]
+        print(f"Nome: {busca.capitalize()} / Preço: {dados['preco']:.2f} / Quantidade: {dados['quantidade']} / Categoria: {dados['categoria']}")
+    else:
+        print("\nProduto não encontrado!\n")
+
+def editar_produto(produto): # Opçao 4
+    menu(False, True)
+    print('\nSempre que quiser chamar a tabela, digite "tabela"!')
+    while True:
+        print("\nDigite o número que deseja ou comando:")
+        escolha = input('= ').lower().strip()
+        print()
+        if escolha == "tabela":
+
+            menu(False, True)
+        
+        elif escolha == "1":
+            print(f"Preço antigo: {estoque[produto]['preco']:.2f}")
+            while True:
+                try:
+                    novo_preco = float(input(f"Digite o novo preço de {produto.capitalize()}: "))
+                except ValueError:
+                    print("\nDigite um valor válido!\n")
+                else:
+                    estoque[produto]["preco"] = novo_preco
+                    print(f"\nPreço de {produto.capitalize()} atulizado!\n")
+                    break
+
+        elif escolha == "2":
+            print(f"Quantidade antiga: {estoque[produto]['quantidade']}")
+            while True:
+                try:
+                    nova_quantidade = int(input(f"Digite a nova quantidade de {produto.capitalize()}: "))
+                except ValueError:
+                    print("\nDigite um valor válido!\n")
+                else:
+                    estoque[produto]["quantidade"] = nova_quantidade
+                    print(f"\nQuantidade de {produto.capitalize()} atualizada!")
+                    break
+
+        elif escolha == "3":
+            print(f"Categoria antiga: {estoque[produto]['categoria']}")
+            nova_categoria = input(f"Digite a nova categoria de {produto.capitalize()}: ")
+
+            estoque[produto]["categoria"] = nova_categoria
+            print(f"\nQuantidade de {produto.capitalize()} atualizada!")
+                
+        elif escolha == "4":
+            print("\nEncerrando...\n")
+            break
+
 
 # Código principal
-menu()
+menu(True, False)
 print('OBS: sempre que quiser chamar a tabela, digite "tabela"!')
 while True:
     print("\nDigite o número que deseja ou comando:")
     escolha = input('= ').lower().strip()
     print()
 
-    if escolha == 'tabela': # CMD = tabela
-        menu()
+    if escolha == 'tabela': # tabela
+        menu(True, False)
 
     elif escolha == 'teste': # TEMPÓRARIO
         print(estoque)
@@ -85,16 +151,33 @@ while True:
 
         lin('*')
         if not estoque:
-            print("Sem produtos no estoque!")
+            print("\nSem produtos no estoque!\n")
         else:
             listar_produtos()
         lin('*')
 
     elif escolha == '3': # Opção 3 (buscar produto pelo nome);
-        pass
+
+        lin('-')
+        if not estoque:
+            print("\nSem produtos no estoque!\n")
+        else:
+            buscar_produto()
+        lin('-')
 
     elif escolha == '4': # Opção 4 (Atualizar preço e quantidade);
-        pass
+
+        lin('-')
+        if not estoque:
+            print("\nSem produtos no estoque!\n")
+        else:
+            busca = input("Digite o nome do produto que deseja editar:\n").lower().strip()
+
+            if busca in estoque:
+                editar_produto(busca)
+            else:
+                print("\nProduto não encontrado!\n")
+        lin('-')
 
     elif escolha == '5': # Opção 5 (Remover produto);
         pass
