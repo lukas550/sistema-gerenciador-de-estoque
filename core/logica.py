7100f494bd95d2a00de98131bb04e3fdc06c8591
Lukas <xd3433660@gmail.com>
2026-09-06 13:45:21 -0300

    feat(core): adiciona módulo logica.py e implementa função de adicionar um produto

---
+++ core/logica.py
@@ -0,0 +1,40 @@
+# Módulo para a lógica do main.py, funções do CRUD, incluindo: Adicionar Produto, Listar Produtos, Buscar Produto, Editar Produto, Excluir Produto e Relatório
+
+def adicionar_produto(nome, preco, quantidade, categoria):
+
+    if not nome.strip() or not categoria.strip():
+        raise ValueError("\nNome e categoria são obrigatório!\n")
+
+    try:
+        preco_float = float(preco)
+        quantidade_int = int(quantidade)
+
+        if preco_float < 0 or quantidade_int < 0:
+            raise ValueError("\nPreço e quantidade não podem ser negativos!\n")
+
+    except (ValueError, TypeError):
+        raise ValueError("\nPreço ou quantidade inválidos!\n")
+
+    produto = {
+        "produto": nome.strip().lower(),
+        "preco": preco_float,
+        "quantidade": quantidade_int,
+        "categoria": categoria.strip().lower()
+    }
+
+    return produto
+
+def listar_produtos():
+    pass
+
+def buscar_produto():
+    pass
+
+def editar_produto():
+    pass
+
+def excluir_produto():
+    pass
+
+def gerar_relatorio():
+    pass
