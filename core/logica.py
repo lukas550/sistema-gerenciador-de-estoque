07cb7bb6222e9048957fa022a3ca2756d5ce55e5
Lukas <xd3433660@gmail.com>
2026-09-06 19:59:29 -0300

    feat(core): adiciona a função de edição no módulo logica.py

--- core/logica.py
+++ core/logica.py
@@ -49,8 +49,43 @@ def buscar_produto(produto_buscado, estoque):
         if not encontrado:
             print(f"\nO produto {produto_buscado} não foi encontrado!\n")
 
-def editar_produto():
-    pass
+def editar_produto(escolha, produto_editado):
+
+    if escolha.strip() == "1":
+        print(f"Preço antigo: {produto_editado['preco']}")
+        try:
+            novo_valor = float(input("\nDigite o novo valor: "))
+
+            if novo_valor < 0:
+                raise ValueError
+
+            produto_editado["preco"] = novo_valor
+            print(f"\nO valor de {produto_editado['produto'].capitalize()} foi atualizado!\n")
+
+        except ValueError:
+            print("\nNovo valor inválido!\n")
+
+    elif escolha.strip() == "2":
+        print(f"Quantidade antiga: {produto_editado['quantidade']}")
+        try:
+            nova_quantidade = int(input("\nDigite a nova quantidade: "))
+
+            if nova_quantidade < 0:
+                raise ValueError
+
+            produto_editado["quantidade"] = nova_quantidade
+            print(f"\nA quantidade de {produto_editado['produto'].capitalize()} foi atualizada!\n")
+
+        except ValueError:
+            print("\nNova quantidade inválida!\n")
+
+    elif escolha.strip() == "3":
+        print(f"Categoria antiga: {produto_editado['categoria']}")
+
+        nova_categoria = input(f"Nova categoria de {produto_editado['produto'].capitalize()}: ").lower().strip()
+
+        produto_editado['categoria'] = nova_categoria
+        print(f"\nA categoria de {produto_editado['produto'].capitalize()} foi atualizada!\n")
 
 def excluir_produto():
     pass
